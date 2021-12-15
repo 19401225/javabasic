09c6982650e8c7a1152eb8ca9c4bf551980eb2ca
19401225 <93993988+19401225@users.noreply.github.com>
2021-12-15 20:20:49 +0900

    Add files via upload

---
+++ Homework/HW1213.java
@@ -0,0 +1,113 @@
+ // HW 제출방법 : https://replit.com/@cainsungu/01HW20211213gangseongu01#Main.java 
+ // URL 주소를 복사하여, 선생님 사이트 > 자바 > 게시판 > HomeWork 
+ // No : 01, 이름을 적고, Link : URL 주소 붙여넣기
+
+
+
+// # 2-7
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    Scanner scan = new Scanner(System.in);
+    i = scan.nextInt();
+
+    System.out.println("정숫값 : " + i);
+
+    j += i / 10;
+    k += i % 10;
+
+    System.out.println("마지막 자릿수를 제외한 값은 " + j +"입니다.");
+    System.out.println("마지막 자릿수는 " + k + "입니다.");
+  
+
+
+// # 2-6
+
+    int a = 0;
+    Scanner sc = new Scanner(System.in);
+    
+    e = sc.nextInt();
+    System.out.println("정숫값 : " + e);
+
+    int b = j + 10;
+    int c = j - 10;
+
+    System.out.println("10을 더한 값은 " + a + "입니다. ");
+    System.out.println("10을 뺀 값은 " + b + "입니다. ");
+  }
+}
+
+
+
+
+
+// # 2-5 
+// import java.util.Scanner;
+// class Main {
+//   public static void main(String[] args) {
+//     int i = 0;
+//     Scanner scan = new Scanner(System.in);
+
+//     i = scan.nextInt();
+//     System.out.println("정숫값 : " + i);
+//     System.out.println(i + "을(를) 입력했습니다. ");
+  
+//   }
+// }
+
+
+
+
+
+// # 2-2
+// import java.util.Scanner;
+// class Main {
+//   public static void main(String[] args) {
+//       int x = 63;
+//       int y = 18;
+//       int sum = x + y;
+//       int avg = (x + y) / 2;
+
+//       System.out.println("x값은 " + x + "입니다. ");
+//       System.out.println("y값은 " + y + "입니다. ");
+//       System.out.println("합계는 " + sum + "입니다. ");
+//       System.out.println("평균은 " + avg + "입니다. ");
+//   }
+// }
+
+
+
+
+// # 1-5 
+// class Main {
+//   public static void main(String[] args) {
+//     String name = "강선구";
+
+//     System.out.println(name.substring(0,1));
+//     System.out.println("");
+//     System.out.println(name.substring(1,2));
+//     System.out.println(name.substring(2,3));
+
+
+
+
+// class Main {
+//   public static void main(String[] args) {
+//     String name = "강선구";
+
+//     char a = name.charAt(0);
+//     char b = name.charAt(1);
+//     char c = name.charAt(2);
+
+//     System.out.println(a);
+//     System.out.println("");
+//     System.out.println(b);
+//     System.out.println(c);
+
+
+//   }
+// }
